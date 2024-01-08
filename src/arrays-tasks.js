61ebe5a580f69dbfc18e0c34246ab385d9d6fa74
ru-ab <ruabdrakhmanov@gmail.com>
2024-01-08 14:22:27 +0600

    feat: implement getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -128,8 +128,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+
+  return Number((arr.reduce((sum, v) => sum + v, 0) / arr.length).toFixed(2));
 }
 
 /**
