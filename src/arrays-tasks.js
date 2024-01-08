6b47852902fe0c02cc5078e0d4a00cbe4ca01a88
ru-ab <ruabdrakhmanov@gmail.com>
2024-01-08 15:06:30 +0600

    feat: implement createChunks function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -342,8 +342,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  if (arr.length <= chunkSize) {
+    return [arr];
+  }
+
+  return [
+    arr.slice(0, chunkSize),
+    ...createChunks(arr.slice(chunkSize), chunkSize),
+  ];
 }
 
 /**
