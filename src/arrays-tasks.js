6930fa3d609cc8568287126764d7f20971edf4f3
ru-ab <ruabdrakhmanov@gmail.com>
2024-01-08 15:25:51 +0600

    feat: implement getHexRGBValues function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -460,8 +460,8 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((v) => `#${v.toString(16).toUpperCase().padStart(6, '0')}`);
 }
 
 /**
